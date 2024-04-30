d8808bee9474909033d1d839e5b6f64dfb7ba22d
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-30 07:11:05 +0000

    pushed youngest and eldest code

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,8 +9,27 @@ class DirectorsController < ApplicationController
     matching_records =  Director.where({ :id => the_id})
 
     @the_director = matching_records.at(0)
-    
+
     render({ :template => "director_templates/details"})
+  end
+
+  def junior
+
+    @youngest_director = Director.where.not({ :dob => nil }).order({ :dob => :asc }).last
+
+    @youngest_director_name = @youngest_director.name
+    @youngest_director_dob = @youngest_director.dob
+
+    render({ :template => "director_templates/junior"})
+  end
+
+  def senior
+    @oldest_director = Director.where.not({ :dob => nil }).order({ :dob => :asc }).first
+
+    @oldest_director_name = @oldest_director.name
+    @oldest_director_dob = @oldest_director.dob
+
+    render({ :template => "director_templates/senior"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
 
+  get("/directors/youngest", { :controller => "directors", :action => "junior" })
+
+  get("/directors/eldest", { :controller => "directors", :action => "senior" })
+
   get("/directors/:an_id", { :controller => "directors", :action => "show" })
 
   get("/movies", { :controller => "movies", :action => "index" })
@@ -12,4 +16,6 @@ Rails.application.routes.draw do
   get("/actors", { :controller => "actors", :action => "index" })
 
   get("/actors/:an_id", { :controller => "actors", :action => "show" })
+
+
 end
