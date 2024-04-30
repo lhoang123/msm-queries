28e4720fa4137a1651ec90024ff78978eac81e7f
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-30 06:02:26 +0000

    code for movies

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
 
   get("/directors/:an_id", { :controller => "directors", :action => "show" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/movies/:an_id", { :controller => "movies", :action => "show" })
 end
